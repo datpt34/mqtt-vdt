9ecc1d6a53810d20b9049bda75043c658177f386
datpt34 <datpt034@gmail.com>
2024-04-25 15:09:14 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+const HOST = "mqttvcloud.innoway.vn"
+const PORT = "1883"
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func main() {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%s", HOST, PORT))
+	opts.SetClientID("go_mqtt_client")
+	opts.SetUsername("emqx")
+	opts.SetPassword("2mlZz20TqGKTevsHOThfZwBeSMa5zn43")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	client.Disconnect(250)
+}
